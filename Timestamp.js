37103942bc98d43c6e03d11befb84cf0be7132b5
unknown <ivusony@gmail.com>
2018-11-25 10:42:31 +0100

    format method added

--- Timestamp.js
+++ Timestamp.js
@@ -20,14 +20,65 @@ Timestamp.prototype.get = function(type, options){
        return this.e[type](options)
     }
 }
+Timestamp.prototype.format = function(opt){
+    switch (opt) {
+        case 't':
+        return this.get('time', 's'); //returns hh:mm
+            break;
+    
+        case 'tt':
+        return this.get('time','l');  //returns hh:mm:ss
+            break;
+
+        case 'ttt':
+        return this.get('time','L'); //returns hh:mm:ss:ms
+            break;
+
+        case 'd':
+        return this.get('day','s');  //returns short version of day
+            break;
+        
+        case 'dd':
+        return this.get('day','l');  //returns long version of day
+            break;
+         
+        case 'dt':
+        return this.get('date');  //returns numeric day of month
+            break;    
+
+        case 'dtms':
+        return `${this.get('date')} ${this.get('month', 's')}`;  //returns numeric day of month folowed by the month short version
+            break;    
+
+        case 'msdt':
+        return `${this.get('month', 's')} ${this.get('date')}`;  //returns the month short version and numeric day of month
+            break;        
+            
+        case 'dtml':
+        return `${this.get('date')} ${this.get('month', 'l')}`;  //returns numeric day of month folowed by the month long version
+            break; 
+
+        case 'mldt':
+        return `${this.get('month', 'l')} ${this.get('date')}`;  //returns the month long version and numeric day of month
+            break;
+
+        case 'mldty':
+        return `${this.get('month', 'l')} ${this.get('date')} ${this.get('year', 'l')}`;  //returns the month long version and numeric day of month
+            break;
+
+        default: 'now'
+        return `${this.get('day','l')} ${this.get('month', 'l')} ${this.get('date')} ${this.get('year', 'l')} ${this.get('time','l')}`;
+            break;
+    }
+}
 //instantiating new object
 let date = new Timestamp();
 
 //defining new query string in the e property of the new object and setting its value to the function
 //which takes only one optional argument, the option how we want to display the name of the day
 date.define('day',function(opt){
-    let d = date.d.getUTCDay();
-    let days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
+    let d = date.d.getUTCDay()+1;
+    let days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
     let dayObj = {
         fullDay : days[d-1],
         short   : days[d-1].substr(0,3)
@@ -58,17 +109,19 @@ date.define('month', function(opt){
     let months = ['January', 'February', 'March', 'April', 'May', 'Jun', 'Jul', 'August', 'Septemper', 'October', 'November', 'December'];
     let mObj =  {
         fullMonth   : months[m],
-        short       : months[m].substr(0,3)
+        short       : months[m].substr(0,3),
+        number      : m
     } 
     if(!opt){
         return  mObj.short
     }else{
         return  opt==='s' ? mObj.short
             :   opt==='l' ? mObj.fullMonth
+            :   opt==='n' ? mObj.number
             :   undefined
     }
 })
-
+//returns short or long version of current year
 date.define('year', function(opt){
     let y = date.d.getFullYear().toString();
     let yObj = {
@@ -83,7 +136,7 @@ date.define('year', function(opt){
             :   undefined
     }
 })
-
+//returns current time
 date.define('time', function(opt){
     let t = {
         h   :   date.d.getHours(),
@@ -91,13 +144,15 @@ date.define('time', function(opt){
         s   :   date.d.getSeconds(),
         ms  :   date.d.getMilliseconds()
     }
-    if(!opt){
-        return `${adjustTime(t.h)}:${adjustTime(t.m)}`
+    if (opt==="s") {
+        return  t.h<12 ? `${adjustTime(t.h)}:${adjustTime(t.m)} AM` : `${adjustTime(t.h)}:${adjustTime(t.m)} PM`
+    }else if(opt==="l"){
+        return  t.h<12 ? `${adjustTime(t.h)}:${adjustTime(t.m)}:${adjustTime(t.s)} AM` : `${adjustTime(t.h)}:${adjustTime(t.m)}:${adjustTime(t.s)} PM`
+    }else if(opt==="L"){
+        return  t.h<12 ? `${adjustTime(t.h)}:${adjustTime(t.m)}:${adjustTime(t.s)}:${t.ms} AM` : `${adjustTime(t.h)}:${adjustTime(t.m)}:${adjustTime(t.s)}:${t.ms} PM`
+    }else{
+        return undefined
     }
-
-    opt.forEach(el => {
-        console.log(t[el])
-    });
     function adjustTime(t){
         if (t < 10) {
             return `0${t}`
@@ -105,31 +160,6 @@ date.define('time', function(opt){
             return t
         }
     }
-   
 })
-
+//exports date object
 module.exports = date
-
-
-// Timestamp.prototype.now = function(){
-//     let dateObj         = new Date();
-//     this.dayOfWeek      = this.returnDay(dateObj.getUTCDay());
-//     this.dateInMonth    = this.returnDate(dateObj.getDate());
-//     this.monthInYear    = this.returnMonth(dateObj.getMonth());
-//     this.year           = dateObj.getFullYear();
-//     this.hours          = this.adjustTime(dateObj.getHours());
-//     this.minutes        = this.adjustTime(dateObj.getMinutes());
-//     this.seconds        = this.adjustTime(dateObj.getSeconds());
-//     this.e = {}
-// }
-
-
-// Timestamp.prototype.on = function(type, fn){
-//     this.e[type] = this.e[type] || [];
-//     this.e[type].push(fn) 
-// }
-
-
-
-
-// module.exports = Timestamp;
--- test.js
+++ test.js
@@ -1,2 +1,9 @@
 let date = require('./Timestamp');
-console.log(date.get('time',['h', 'm']))
+// console.log(date.get('day', 'l'));
+// console.log(date.get('date'));
+// console.log(date.get('month', 'l'));
+// console.log(date.get('year', 'l'));
+// console.log(date.get('time'));
+
+console.log(date.format());
+
